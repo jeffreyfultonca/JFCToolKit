913385ab58e4335d12c8f8f5a85da95ff5363ffd
Jeffrey Fulton <jeffrey.fulton@me.com>
2017-05-04 12:28:45 -0500

    Add empty Xcode framework project.

---
+++ JFCToolKit/JFCToolKit.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for JFCToolKit.
+FOUNDATION_EXPORT double JFCToolKitVersionNumber;
+
+//! Project version string for JFCToolKit.
+FOUNDATION_EXPORT const unsigned char JFCToolKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JFCToolKit/PublicHeader.h>
